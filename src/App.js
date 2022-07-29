70bc5c09023c78c5d9dc2663286d0bd6fc2c825b
Malinda Anupama <55244908+malinda666@users.noreply.github.com>
2022-07-29 23:50:30 +0530

    add flowbite + tailwind + fabric

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
 
 export default function App() {
-  return <Outlet />
+  // return <Outlet />
+  const { editor, onReady } = useFabricJSEditor()
+  const onAddCircle = () => {
+    editor?.addCircle()
+  }
+  const onAddRectangle = () => {
+    editor?.addRectangle()
+  }
+
+  return (
+    <div className="max-w-xl mx-auto">
+      <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
+    </div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import RouteMap from './components/common/RouteMap'
+import App from 'App'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouteMap />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
