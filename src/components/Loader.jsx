31161376ad57c1ceb2e66f398b80b9c69df765b6
Malinda Anupama <55244908+malinda666@users.noreply.github.com>
2022-08-13 01:05:27 +0530

    lint + format

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,3 @@
-import Spinner from './Spinner'
-
 const Loader = ({ isLoading }) => {
   return isLoading ? (
     <div className='fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50' />
